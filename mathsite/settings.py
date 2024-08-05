0cf4a784ddf6a6b35e17c39a66fed1f8c88be609
toonarmycaptain <toonarmycaptain@hotmail.com>
2024-08-04 21:46:45 -0500

    Formatting fixes

--- mathsite/settings.py
+++ mathsite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -25,8 +24,10 @@ SECRET_KEY = "somesecretkeythatshouldbeverydifficulttoguessorcompletelyrandom"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]  # host name eg pythonanywhere.com, your url
-
+ALLOWED_HOSTS = [
+    "192.168.1.95",
+    "127.0.0.1",
+]  # host name eg pythonanywhere.com, your url
 
 # Application definition
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "kolo.middleware.KoloMiddleware",
 ]
 
 ROOT_URLCONF = "mathsite.urls"
@@ -70,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "mathsite.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +81,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +110,6 @@ TIME_ZONE = "America/Chicago"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
